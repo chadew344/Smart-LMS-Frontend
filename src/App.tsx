777e0fe639b8114085bd0d863ba3cf7eca5594a9
Chanuth Dewhan <chanuthdewhan273@gmail.com>
2025-12-06 01:13:01 +0530

    Initial commit: Project setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-amber-800">
+        Smart Learnging Management System
+      </h1>
+    </>
+  );
+};
+
+export default App;
